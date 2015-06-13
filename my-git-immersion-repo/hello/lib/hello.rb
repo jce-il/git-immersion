8fc868c575352a94e4de37bf6ce4a0acd1c025fc
razco <rcm1_2@hotmail.com>
2015-06-13 12:15:05 +0300

    Hello uses Greeter

--- my-git-immersion-repo/hello/lib/hello.rb
+++ my-git-immersion-repo/hello/lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Raz Cohen (raz@somewhere.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
