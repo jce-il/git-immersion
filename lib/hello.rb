ef4578689b4612400e07e41ed1949b5ffe5acf81
katyadenef <sywriter.nef@gmail.com>
2016-03-10 17:26:55 +0200

    made intentional conflict

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
-# Default is World
-# Author: Katya Denef (skywriter.nef@gmail.com)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!
+puts "Hello, #{my_name}!"
